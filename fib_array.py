3ca0e0322cba8c31c953ae25d24a17f6627e5981
sttstts <118021225+sttstts@users.noreply.github.com>
2024-12-23 00:31:25 +0300

    1.3

---
+++ fib_array.py
@@ -0,0 +1,23 @@
+def fib(n):
+    if n < 1 or n > 40:
+        raise ValueError("n должно быть в пределах от 1 до 40")
+
+    fib_sequence = [0] * (n + 1)
+
+    fib_sequence[0] = 0
+    if n > 0:
+        fib_sequence[1] = 1
+
+    for i in range(2, n + 1):
+        fib_sequence[i] = fib_sequence[i - 1] + fib_sequence[i - 2]
+
+    print(fib_sequence)
+    return fib_sequence
+
+# Пример вызова функции
+if __name__ == "__main__":
+    try:
+        n = int(input("Введите число n (от 1 до 40): "))
+        fib(n)
+    except ValueError as e:
+        print(e)
