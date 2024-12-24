77784f0b216f023162c07cb59ab05c6d979a93dc
sttstts <118021225+sttstts@users.noreply.github.com>
2024-12-25 02:07:43 +0300

    1.5

---
+++ fib_big_even_odd.py
@@ -0,0 +1,21 @@
+def fib_eo(n):
+    a, b = 0, 1
+
+    for _ in range(n):
+        a, b = b, (a + b) % 10
+
+    if a % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+
+if __name__ == "__main__":
+    try:
+        n = int(input("Введите число n (1 ≤ n ≤ 10^6): "))
+        if 1 <= n <= 10 ** 6:
+            fib_eo(n)
+        else:
+            print("Ошибка: число должно быть в диапазоне от 1 до 10^6.")
+    except ValueError:
+        print("Ошибка: введите корректное целое число.")
